aeb45796728a4c8ee44542cf4ad5d2fe2059c604
Surafel Melese <surafelmelese09@gmail.com>
2022-03-14 13:19:04 +0300

    only the card list remains

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { DrawerComponent } from './component/drawer/drawer.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { ContactHeaderComponent } from './component/contact-header/contact-header.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawerComponent,
+    NavbarComponent,
+    ContactHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
